1ff17bc5a55824c0ec79aa54e63dba6b9fd8294e
Geraldine Forto <geraldineforto@gmail.com>
2016-07-19 11:13:39 +0800

    MainPage_UI

---
+++ VaingloryGuide/VaingloryGuide/CustomCollectionCell/CustomCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCollectionViewCell.swift
+//  VaingloryGuide
+//
+//  Created by Geraldine Forto on 19/07/2016.
+//  Copyright © 2016 mundomo. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
